279ea07244b7b68c6c0b23241cbe1d6b420be50b
casperliuliuliu <casperliu1118@gmail.com>
2024-01-29 14:53:50 +0800

    add unit conversion

--- TOOLs/sum_duration.py
+++ TOOLs/sum_duration.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 import os
 def sum_audio_durations(folder_path):
     total_duration = 0
-    AudioSegment.converter = r"C:/ffmpeg/bin/ffmpeg.exe"  # Update this path as needed
+    # AudioSegment.converter = r"C:/ffmpeg/bin/ffmpeg.exe"  # Update this path as needed
 
     for filename in os.listdir(folder_path):
         if filename.lower().endswith(('.wav', '.mp3', '.flac', '.aac', '.ogg', '.m4a')):  # Add other formats if needed
@@ -12,7 +12,32 @@ def sum_audio_durations(folder_path):
 
     return total_duration / 1000  # Convert milliseconds to seconds
 
-if __name__ == "main":
+def convert_seconds(total_seconds):
+    time_components = []
+    days = total_seconds // (24 * 3600)
+    if days > 0:
+        time_components.append(f"{int(days)} day")
+
+    total_seconds = total_seconds % (24 * 3600)
+    hours = total_seconds // 3600
+    if hours > 0:
+        time_components.append(f"{int(hours)} hr")
+
+    total_seconds %= 3600
+    minutes = total_seconds // 60
+    if minutes > 0:
+        time_components.append(f"{int(minutes)} min")
+
+    seconds = total_seconds % 60
+    if seconds > 0:
+        time_components.append(f"{seconds:.1f} sec")
+
+    formatted_time = " ".join(time_components)
+    print(formatted_time)
+
+
+
+if __name__ == "__main__":
     folder_path = 'D:/Casper/Weight/test'
     total_duration_seconds = sum_audio_durations(folder_path)
-    print(f"Total audio duration: {total_duration_seconds} seconds")
+    convert_seconds(total_duration_seconds)
