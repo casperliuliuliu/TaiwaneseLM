b0eed25deaa2db19cb15b01ccb5b91ac1f096a8f
casperliuliuliu <casperliu1118@gmail.com>
2024-02-04 20:19:26 +0800

    testing whisper

--- ASR/Whisper/deploy.py
+++ ASR/Whisper/deploy.py
@@ -1,5 +1,5 @@
 import whisper
 
 model = whisper.load_model("base")
-result = model.transcribe("D:/Casper/Weight/clip_1.wav")
+result = model.transcribe("D:/Casper/OTHER/Weight/test/clip_19.wav", language='zh')
 print(result["text"])
