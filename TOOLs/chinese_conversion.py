226d8ae605aa3048912f0784f8d54efefe121e1d
casperliuliuliu <casperliu1118@gmail.com>
2024-02-07 06:24:49 +0800

    update phrase conversion

--- TOOLs/chinese_conversion.py
+++ TOOLs/chinese_conversion.py
@@ -12,6 +12,20 @@ def tw2s(to_convert):
     converted = cc.convert(to_convert)
     return converted
 
+def s2twp(to_convert):
+    cc = OpenCC('s2twp') 
+    converted = cc.convert(to_convert)
+    return converted
+
+def tw2sp(to_convert):
+    cc = OpenCC('tw2sp')
+    converted = cc.convert(to_convert)
+    return converted
+
 if __name__ == "__main__":
-    print(tw2s("開放中文轉換"))
-    print(s2tw("开放中文转换"))
+    print(tw2s("開放中文轉換，馬鈴薯"))
+    print(s2tw("开放中文转换，土豆"))
+
+    # Phrase conversion is not working 
+    print(tw2sp("開放中文轉換，馬鈴薯"))
+    print(s2twp("开放中文转换，土豆"))
