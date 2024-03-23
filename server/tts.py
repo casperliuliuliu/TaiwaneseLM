7bae79e7d845f77f81b5436e343ffdff868ac684
Casperliuliuliu <casperliu1118@gmail.com>
2024-03-23 09:11:52 +0800

    some test audio

--- server/tts.py
+++ server/tts.py
@@ -2,8 +2,8 @@
 Example code for yating tts api call.
 """
 import sys
-desired_path = "D:/Casper/Other"
-# desired_path = "/Users/liushiwen/Desktop/大四下"
+# desired_path = "D:/Casper/Other"
+desired_path = "/Users/liushiwen/Desktop/大四下"
 
 sys.path.append(desired_path)
 from my_config import get_config
@@ -13,7 +13,6 @@ from yating_tts_sdk import YatingClient as ttsClient
 
 
 def speak_word_with_yating(word, store_path = '', model_name=ttsClient.MODEL_TAI_FEMALE_1):
-    URL = "TTS_ENDPOINT"
     URL = "https://tts.api.yating.tw/v2/speeches/short"
     KEY = yating_key
 
@@ -21,7 +20,7 @@ def speak_word_with_yating(word, store_path = '', model_name=ttsClient.MODEL_TAI
     TEXT_TYPE = ttsClient.TYPE_TEXT
     MODEL = model_name
     SPEED = 1.0
-    PITCH = 1.0
+    PITCH = 1.3
     ENERGY = 1.5
     ENCODING = ttsClient.ENCODING_MP3
     SAMPLE_RATE = ttsClient.SAMPLE_RATE_16K
@@ -35,5 +34,5 @@ def speak_word_with_yating(word, store_path = '', model_name=ttsClient.MODEL_TAI
 
 if __name__ == "__main__":
     store_path = "/Users/liushiwen/Desktop/大四下/NSC/TaiwaneseLM/server/yating/"
-    speak_word_with_yating("酷大便", store_path, ttsClient.MODEL_TAI_FEMALE_1)
-    speak_word_with_yating("酷大便", store_path, ttsClient.MODEL_ZHEN_FEMALE_1)
+    speak_word_with_yating("酷大便，三", store_path, ttsClient.MODEL_TAI_FEMALE_1)
+    speak_word_with_yating("酷大便，四", store_path, ttsClient.MODEL_ZHEN_FEMALE_1)
